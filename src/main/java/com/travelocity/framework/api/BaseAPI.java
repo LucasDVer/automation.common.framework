17a4ea680a800d4e7212922e07afe5e55f02cdd2
LucasDVer <lucasverond@gmail.com>
2020-02-25 21:00:44 -0300

    Updated scope of RestAssured dependency. Added two new methods to read properties from file. Added a base api class

---
+++ src/main/java/com/travelocity/framework/api/BaseAPI.java
@@ -0,0 +1,36 @@
+package com.travelocity.framework.api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public abstract class BaseAPI {
+
+    private String baseUrl;
+    private String specificPath = "";
+    private String query = "";
+
+    public BaseAPI() {
+        loadBaseUrl();
+    }
+
+    public Response doGet() {
+        return RestAssured.get(baseUrl + specificPath + query);
+    }
+
+    protected abstract void loadBaseUrl();
+
+    protected void loadSpecificPath() {
+    }
+
+    public void setBaseUrl(String url) {
+        baseUrl = url;
+    }
+
+    public void setSpecificPath(String specificPath) {
+        this.specificPath = specificPath;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
--- src/main/java/com/travelocity/framework/utils/ConfigUtils.java
+++ src/main/java/com/travelocity/framework/utils/ConfigUtils.java
@@ -4,20 +4,43 @@ import com.travelocity.framework.configuration.ConfigFile;
 import com.travelocity.framework.logger.Loggable;
 import com.travelocity.framework.ui.browser.Browsers;
 import com.travelocity.framework.ui.platform.Platform;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.Properties;
 
 import static java.lang.System.getProperty;
 import static java.lang.System.getenv;
 
 public class ConfigUtils implements Loggable {
 
-    // Environment variable names
     private static final String ENVIRONMENT = "environment";
     private static final String DEFAULT_ENVIRONMENT = "automation";
     private static final String PLATFORM = "platform";
     private static final String BROWSER = "browser";
     private static final String CONFIG_FILE_SUFFIX = "config";
+    private static Properties properties;
 
     private ConfigUtils() {
+    }
+
+    private static Properties getInstance() {
+        if (properties == null) {
+            try {
+                return FileUtils.loadFromProperties(ConfigUtils.getEnvironment());
+            } catch (IOException e) {
+                throw new RuntimeException("Property file was not found");
+            }
+        }
+        return properties;
+    }
+
+    public static String getPropertyFromFile(String key) {
+        String property = getPropertyValue(key);
+        if (StringUtils.isBlank(getProperty(key))) {
+            return getInstance().getProperty(key);
+        }
+        return property;
     }
 
     public static String getPropertyValue(String key) {
