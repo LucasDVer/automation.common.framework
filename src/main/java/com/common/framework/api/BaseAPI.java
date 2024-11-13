b33a229fa1eb73af914a56b59712ca5658e24a12
LucasDVer <lucasverond@gmail.com>
2024-11-13 18:02:34 -0300

    Updating repo

--- src/main/java/com/common/framework/api/BaseAPI.java
+++ src/main/java/com/common/framework/api/BaseAPI.java
@@ -5,40 +5,25 @@ import io.restassured.response.Response;
 
 public abstract class BaseAPI {
 
-    private String baseUrl;
-    private String specificPath = "";
-    private String query = "";
+    private static String baseUrl;
+    private static String specificPath = "";
 
     protected BaseAPI() {
         loadBaseUrl();
         loadSpecificPath();
     }
 
-    public Response doGet() {
-        return RestAssured.get(baseUrl + specificPath + query);
-    }
+    protected  Response doGet(String path) { return RestAssured.get(baseUrl + specificPath + path); }
 
     protected abstract void loadBaseUrl();
 
     protected abstract void loadSpecificPath();
 
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
     public void setBaseUrl(String url) {
         baseUrl = url;
     }
 
-    public String getSpecificPath() {
-        return specificPath;
-    }
-
     public void setSpecificPath(String specificPath) {
         this.specificPath = specificPath;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
     }
 }
